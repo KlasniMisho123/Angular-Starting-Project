d5e5a4f969d9682089540b99bb51c790b4b21a9f
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-25 23:23:34 +0400

    directives

---
+++ src/app/components/master/master.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { DesignationComponent } from '../designation/designation.component';
+import { RolesComponent } from '../roles/roles.component';
+
+@Component({
+  selector: 'app-master',
+  imports: [DesignationComponent, RolesComponent],
+  templateUrl: './master.component.html',
+  styleUrl: './master.component.css'
+})
+export class MasterComponent {
+  currentComponent:string = "";
+}
