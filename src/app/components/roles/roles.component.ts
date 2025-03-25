737fd9b144f1d9b250ac4647e86d39962ccbb104
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-25 16:29:50 +0400

    components setup

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
+  firstName: string = "Starting Angular Project";
+  angularVersion = "Version 19.2.4";
+  
+  versionNumber = 19.2;
 
+  isActive: boolean = false;
+
+  currentDate: Date = new Date();
 }
