3a0880de73837a7ec0599d0cbf6a996b5e99cf7b
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-25 17:31:02 +0400

    bootstrap commit

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
   firstName: string = "Starting Angular Project";
   angularVersion = "Version 19.2.4";
-  
+
   versionNumber = 19.2;
-
   isActive: boolean = false;
-
   currentDate: Date = new Date();
+  inputType: string = "checkbox";
+  selectedState: string = '';
+
+  buttonClicked():void {
+    alert("GOT CLICKED")
+  }
+  buttonMessage(message: string):void {
+    alert(message)
+  }
 }
