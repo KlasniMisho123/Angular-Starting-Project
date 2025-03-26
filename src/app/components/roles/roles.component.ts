1b5796f5be15e98393eaa2c8d8e0df7689bed35e
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-26 15:31:46 +0400

    oninit commit

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../../model/interface/role';
 
 @Component({
   selector: 'app-roles',
@@ -9,21 +11,30 @@ import { FormsModule } from '@angular/forms';
 })
 export class RolesComponent implements OnInit{
   
+  roleList: IRole [] = [];
+  http = inject(HttpClient)
+
   ngOnInit(): void {
-    console.log('hi')
+    this.getAllRoles()
+  }
+
+  getAllRoles() {
+    this.http.get("https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles").subscribe((res: any)=>{
+      this.roleList = res.data;
+    })
   }
   
-  firstName: string = "Starting Angular Project";
+  // firstName: string = "Starting Angular Project";
 
-  versionNumber = 19.2;
-  currentDate: Date = new Date();
-  inputType: string = "checkbox";
-  selectedState: string = '';
+  // versionNumber = 19.2;
+  // currentDate: Date = new Date();
+  // inputType: string = "checkbox";
+  // selectedState: string = '';
 
-  buttonClicked():void {
-    alert("GOT CLICKED")
-  }
-  buttonMessage(message: string):void {
-    alert(message)
-  }
+  // buttonClicked():void {
+  //   alert("GOT CLICKED")
+  // }
+  // buttonMessage(message: string):void {
+  //   alert(message)
+  // }
 }
