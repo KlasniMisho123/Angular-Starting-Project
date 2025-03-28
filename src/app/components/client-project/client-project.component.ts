0eb52a0d5b0e437ee92b11538268a5b026f157c5
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-28 21:31:44 +0400

    onSaveProject commit

--- src/app/components/client-project/client-project.component.ts
+++ src/app/components/client-project/client-project.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ClientService } from '../../services/client.service';
 import { APIResponseModel, Employee } from '../../model/interface/role';
 import { Client } from '../../model/class/Client';
@@ -14,7 +14,7 @@ export class ClientProjectComponent implements OnInit{
   
   projectForm: FormGroup = new FormGroup({
     clientProjectId: new FormControl(0),
-    projectName: new FormControl(""),
+    projectName: new FormControl("", [Validators.required,Validators.minLength(4)]),
     startDate: new FormControl(""),
     expectedEndDate:  new FormControl(""),
     leadByEmpId: new FormControl(0),
