52ddaa91616c31ceb013f8e8f4d83b3ad7c38459
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-28 16:48:19 +0400

    Form

--- src/app/components/client-project/client-project.component.ts
+++ src/app/components/client-project/client-project.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-client-project',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './client-project.component.html',
   styleUrl: './client-project.component.css'
 })
 export class ClientProjectComponent {
 
+  projectForm: FormGroup = new FormGroup({
+    clientProjectId: new FormControl(0),
+    projectName: new FormControl(""),
+    startDate: new FormControl(""),
+    expectedEndDate:  new FormControl(""),
+    leadByEmpId: new FormControl(0),
+    completedDate:  new FormControl(""),
+    contactPerson:  new FormControl(""),
+    contactPersonContactNo:  new FormControl(""),
+    totalEmpWorking: new FormControl(""),
+    projectCost: new FormControl(""),
+    projectDetails:  new FormControl(""),
+    contactPersonEmailId:  new FormControl(""),
+    clientId: new FormControl("")
+  })
 }
