c208fb5ed5019f19ef55976b8b60d722dba2e900
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-29 23:40:13 +0400

    login

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -9,4 +9,10 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 })
 export class LayoutComponent {
 
+  router = inject(Router)
+  logOut() {
+    this.router.navigateByUrl('/login')
+    localStorage.removeItem('empErpUser')
+    alert("User Logged Out!")
+  }
 }
